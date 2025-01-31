cf1bbcc332f7c7eae1c9fafdf4c344d817d73caf
javstash <admin@javstash.org>
2025-01-31 03:04:29 -0500

    add decensoring

--- scrapers/R18dev_SQL/R18dev_SQL.py
+++ scrapers/R18dev_SQL/R18dev_SQL.py
@@ -1,14 +1,22 @@
 # -*- coding: UTF-8 -*-
 # Set Language
 LANG='EN' # JA or EN
+service_code = '%'
+
 # Uncomment the following line to force service_code='digital'
 # service_code = 'digital'
+
 # Uncomment the following line to force service_code='mono'
 # service_code = 'mono' 
 
+# If True, uses label instead of maker for studio (eg: Moodyz Acid instead of Moodyz)
+use_label_as_studio = False
+
 # Uncomment to enable logging (lol)
 #import py_common.log as log
-import sys, json, re
+
+
+import sys, json, re, csv
 import psycopg2
 conn = psycopg2.connect(database="r18",
                         host="localhost",
@@ -32,7 +40,7 @@ def get_content_id(dvd_code, service_code='%'):
 def get_scene_info(content_id, service_code='%'):
     cursor = conn.cursor()
     cursor.execute(f"""
-                    SELECT title_ja, title_en, MT.target_en, comment_ja, comment_en, release_date, jacket_full_url, maker_id, series_id, dvd_id, service_code
+                    SELECT title_ja, title_en, MT.target_en, comment_ja, comment_en, release_date, jacket_full_url, maker_id, label_id, series_id, dvd_id, service_code
                     FROM derived_video 
                     LEFT JOIN machine_translation MT ON derived_video.title_ja = MT.source_ja
                     WHERE content_id='{content_id}' AND service_code like '{service_code}' 
@@ -99,6 +107,18 @@ def get_studio(maker_id):
 
     return result
 
+def get_label(label_id):
+    cursor = conn.cursor()
+    cursor.execute(f"""
+                    SELECT L.name_ja, L.name_en, MT.target_en FROM public.derived_label L
+                    LEFT JOIN machine_translation MT ON L.name_ja = MT.source_ja
+                    WHERE L.id = '{label_id}'
+                   """)
+    result = cursor.fetchone()
+    cursor.close()
+
+    return result
+
 def get_series(series_id):
     cursor = conn.cursor()
     cursor.execute(f"""
@@ -110,6 +130,15 @@ def get_series(series_id):
     cursor.close()
 
     return result
+
+def decensor(string):
+    if string is None:
+        return None
+    with open('decensor.csv', 'r') as decensor_file:
+        decensor_reader = csv.reader(decensor_file)
+        for row_decensor in decensor_reader:
+            string = string.replace(row_decensor[0],row_decensor[1])
+    return string
 
 SUPER_DUPER_JAV_CODE_REGEX = r'.*?([a-zA-Z|tT28|tT38]+)-?(\d+)[zZ]?[eE]?(?:-pt)?(\d{1,2})?.*'
 
@@ -132,8 +161,8 @@ elif (sys.argv[1] == "sceneByQueryFragment" or sys.argv[1] == "sceneByFragment")
         pass
     try:
         input_code = i['code']
-        if(re.search(SUPER_DUPER_JAV_CODE_REGEX,input_title)):
-            dvd_code = re.search(SUPER_DUPER_JAV_CODE_REGEX,input_title).group(1)+'-'+re.search(SUPER_DUPER_JAV_CODE_REGEX,input_title).group(2)
+        if(re.search(SUPER_DUPER_JAV_CODE_REGEX,input_code)):
+            dvd_code = re.search(SUPER_DUPER_JAV_CODE_REGEX,input_code).group(1)+'-'+re.search(SUPER_DUPER_JAV_CODE_REGEX,input_code).group(2)
             dvd_code_found = True
     except:
         pass
@@ -151,15 +180,15 @@ elif (sys.argv[1] == "sceneByURL"):
         content_id_found = True
 
 if(content_id_found == False):
-    content_ids = get_content_id(dvd_code)
+    content_ids = get_content_id(dvd_code, service_code)
     content_id = content_ids[0][1]
 
-scene_info = get_scene_info(content_id)
+scene_info = get_scene_info(content_id, service_code)
 
 title_ja = scene_info[0]
-title_en = scene_info[2] if scene_info[1] is None else scene_info[1]
+title_en = decensor(scene_info[2]) if scene_info[1] is None else decensor(scene_info[1])
 details_ja = scene_info[3]
-details_en = scene_info[4]
+details_en = decensor(scene_info[4])
 date = scene_info[5].strftime("%Y-%m-%d")
 url = "https://r18.dev/videos/vod/movies/detail/-/id="+content_id+"/"
 service_code = scene_info[10]
@@ -169,9 +198,11 @@ else: # assume mono
     image = "https://awsimgsrc.dmm.com/dig/"+scene_info[6].replace('adult/','')+".jpg"
     # Old DMM endpoint
     #image = "https://pics.dmm.co.jp/"+scene_info[6]+".jpg"
+
 maker_id = scene_info[7]
-series_id = scene_info[8]
-code = scene_info[9]
+label_id = scene_info[8]
+series_id = scene_info[9]
+code = scene_info[10]
     
 actress_info = get_actress_info(content_id)
 actress_ja = [{'name': i[0]} for i in actress_info]
@@ -189,7 +220,7 @@ director_en = ', '.join(director_en)
 
 tags_info = get_tags(content_id)
 tags_ja = [{'name': i[0]} for i in tags_info]
-tags_en = [{'name': i[2]} if i[1] is None else {'name': i[1]} for i in tags_info]
+tags_en = [{'name': decensor(i[2])} if i[1] is None else {'name': decensor(i[1])} for i in tags_info]
 tags_en = [{'name': i['name']} if j['name'] is None else {'name': j['name']} for i,j in zip(tags_ja,tags_en)]
 
 studio_info = get_studio(maker_id)
@@ -197,8 +228,13 @@ studio_ja = {'name': studio_info[0]}
 studio_en = {'name': studio_info[2]} if studio_info[1] is None else {'name': studio_info[1]}
 studio_en = {'name': studio_info[0]} if studio_en['name'] is None else studio_en
 
-#if (series_id is None): Need to fix
-if (True):
+if(use_label_as_studio):
+    label_info = get_label(label_id)
+    label_ja = {'name': label_info[0]}
+    label_en = {'name': label_info[2]} if label_info[1] is None else {'name': label_info[1]}
+    label_en = {'name': label_info[0]} if label_en['name'] is None else label_en
+
+if (series_id is None):
     series_ja = None
     series_en = None
 else:
@@ -226,10 +262,13 @@ if (LANG == 'JA'):
         res["director"] = director_ja
     if tags_ja is not None:
         res["tags"] = tags_ja
-    if studio_ja is not None:
-        res["studio"] = studio_ja
+    if use_label_as_studio and label_ja is not None:
+        res["studio"] = label_ja
+    else:
+        if studio_ja is not None:
+            res["studio"] = studio_ja
     if series_ja is not None:
-        res["groups"] = series_ja
+        res["groups"] = [series_ja]
 elif (LANG == 'EN'):
     if title_en is not None:
         res["title"] = title_en
@@ -241,11 +280,15 @@ elif (LANG == 'EN'):
         res["director"] = director_en
     if tags_en is not None:
         res["tags"] = tags_en
-    if studio_en is not None:
-        res["studio"] = studio_en
+    if use_label_as_studio and label_en is not None:
+        res["studio"] = label_en
+    else:
+        if studio_en is not None:
+            res["studio"] = studio_en
     if series_en is not None:
-        res["groups"] = series_en
+        res["groups"] = [series_en]
 
-print(json.dumps(res))
 conn.close()
+print(json.dumps(res))
+
 
